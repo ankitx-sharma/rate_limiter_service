947ea20aadb18c12c9a5cd0fa6190fd1ba52217f
Ankita Sharma <ankitasharma.281993@gmail.com>
2025-12-30 17:38:58 +0100

    changes to allow swagger apis in spring security

--- src/main/java/com/project/rate_limiter/filter/RateLimiterFilter.java
+++ src/main/java/com/project/rate_limiter/filter/RateLimiterFilter.java
@@ -2,13 +2,10 @@ package com.project.rate_limiter.filter;
 
 import java.io.IOException;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import com.project.rate_limiter.service.FixedSizeRateLimiterService;
-import com.project.rate_limiter.service.SlidingWindowRateLimiterService;
 import com.project.rate_limiter.service.TokenBucketRateLimiterService;
 
 import jakarta.servlet.FilterChain;
@@ -18,26 +15,29 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 public class RateLimiterFilter extends OncePerRequestFilter{
-
-//	@Autowired
-//	private FixedSizeRateLimiterService service;
 	
-//	@Autowired
-//	private SlidingWindowRateLimiterService service;
+	private TokenBucketRateLimiterService service = new TokenBucketRateLimiterService();
 	
-	@Autowired
-	private TokenBucketRateLimiterService service;
-
 	@Override
-	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-			throws ServletException, IOException {
+	protected void doFilterInternal(HttpServletRequest request, 
+			HttpServletResponse response, 
+			FilterChain filterChain) throws ServletException, IOException {
 		
+
 		if(!service.isAllowed(request.getRemoteAddr())) {
 			response.setStatus(HttpStatus.TOO_MANY_REQUESTS.value());
 			return;
 		}
 		
 		filterChain.doFilter(request, response);
+	}
+	
+	@Override
+	protected boolean shouldNotFilter(HttpServletRequest request) {
+		String path = request.getRequestURI();
+		
+		return path.startsWith("/swagger-ui") || 
+			   path.startsWith("/v3/api-docs");
 	}
 	
 
